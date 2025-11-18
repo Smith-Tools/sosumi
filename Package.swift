9854e0fd910b3695df18b6ed6adfe7e3447a1e92
Cristian Díaz <cristian@210x7.io>
2025-11-18 17:39:10 +0100

    Pin swift-testing to 0.9.x

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-testing.git", from: "0.9.0"),
+        .package(url: "https://github.com/apple/swift-testing.git", .upToNextMinor(from: "0.9.0")),
     ],
     targets: [
         .executableTarget(
